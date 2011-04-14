e41fb3191d641553362fbb6e8e6404f2421200e2
William Morgan <william@twitter.com>
2011-04-14 16:43:57 -0700

    bump sbt-thrift dep in finagle
    (sapling split of 501e8482b5510fa7706d3953a832bc9780c2cd76)

--- birdcage/finagle/finagle-stream/src/main/scala/com/twitter/finagle/stream/Stream.scala
+++ birdcage/finagle/finagle-stream/src/main/scala/com/twitter/finagle/stream/Stream.scala
@@ -1,18 +1,15 @@
 package com.twitter.finagle.stream
 
-import com.twitter.concurrent.Channel
 import com.twitter.finagle.{Codec, ClientCodec, ServerCodec}
+import org.jboss.netty.channel._
+import org.jboss.netty.handler.codec.http.{HttpServerCodec, HttpClientCodec, HttpRequest}
 import org.jboss.netty.buffer.ChannelBuffer
-import org.jboss.netty.channel.{ChannelPipelineFactory, Channels}
-import org.jboss.netty.handler.codec.http.{HttpServerCodec, HttpClientCodec, HttpRequest, HttpResponse}
 
 object Stream extends Stream
 
-case class StreamResponse(httpResponse: HttpResponse, channel: Channel[ChannelBuffer])
-
-class Stream extends Codec[HttpRequest, StreamResponse] {
+class Stream extends Codec[HttpRequest, com.twitter.concurrent.Channel[ChannelBuffer]] {
   override def serverCodec =
-    new ServerCodec[HttpRequest, StreamResponse] {
+    new ServerCodec[HttpRequest, com.twitter.concurrent.Channel[ChannelBuffer]] {
       def pipelineFactory = new ChannelPipelineFactory {
         def getPipeline = {
           val pipeline = Channels.pipeline()
@@ -24,7 +21,7 @@ class Stream extends Codec[HttpRequest, StreamResponse] {
     }
 
   override def clientCodec =
-    new ClientCodec[HttpRequest, StreamResponse] {
+    new ClientCodec[HttpRequest, com.twitter.concurrent.Channel[ChannelBuffer]] {
       def pipelineFactory = new ChannelPipelineFactory {
         def getPipeline = {
           val pipeline = Channels.pipeline()
