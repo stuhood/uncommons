1aa39d1289c76c707bc8c77678f87b2d5490f0e5
Johan Oskarsson <johan@oskarsson.nu>
2011-04-26 17:44:18 -0700

    Catch and count exceptions properly
    (sapling split of 79de79e7350f192f99854ac309c511a4d9fe5324)

--- birdcage/finagle/finagle-core/src/test/scala/com/twitter/finagle/loadbalancer/LeastQueuedStrategySpec.scala
+++ birdcage/finagle/finagle-core/src/test/scala/com/twitter/finagle/loadbalancer/LeastQueuedStrategySpec.scala
@@ -29,27 +29,24 @@ object LeastQueuedStrategySpec extends Specification with Mockito {
     }
   }
 
-  "LeastWeightedStrategy" should {
+  "LeastQueuedStrategy" should {
     val s0 = new FixedCapacityService[Int](1)
     val s1 = new FixedCapacityService[Int](10)
     val services = Seq(s0, s1)
     val pools = services map { new SingletonFactory(_) }
-    val strategy = new LeastQueuedStrategy()
-    val balancer = new LoadBalancedFactory(pools, strategy)
+    val strategy = new LeastQueuedStrategy[Int, Int]()
 
-    "assign weight according to capacity" in {
+    "distribute load according to capacity" in {
       0 until 100000 foreach { i =>
-        if (i % 10 == 0) {
+        if (i % 10 == 0)
           services foreach { _.tick() }
-        }
 
-        val service: Service[Int, Int] = balancer.make()()
+        val service = strategy(pools)()
         service(123) ensure { service.release() }
       }
 
       s0.count + s1.count must be_==(100000)
       s0.count.toDouble / 100000.0 must beCloseTo(1.0 / 11.0, 0.02)
-      s1.count.toDouble / 100000.0 must beCloseTo(10.0 / 11.0, 0.02)
     }
   }
 }
