42d8d64397665e8210be542a71fd8b23212c527d
marius a. eriksen <marius@twitter.com>
2011-06-15 21:27:27 -0700

    introduce future cancellation via: Future.cancel(), Future.onCancellation().  the change is best described by the comment for cancel():
      /**
       * Cancel the computation.  This is a hint: the provider of the
       * value may not be listening.  Thus, the semantics of cancellation
       * are special.  Upon invoking cancel():
       *
       *  - if the Future is already satisfied, this is a no-op
       *  - if the Future is still waiting, we throw a Future.CancelledException
       *
       * This implies the following invariants (still) hold:
       *
       *  - Futures are respond-at-most-once
       *  - Futures are update-at-most-once
       *
       * This in turn means that semantics change in the face of
       * cancellation only in the presence of extra-Future coordination
       * that depends on Future update/respond semantics.  This style is
       * discouraged anyway, and likely indicates that a different
       * coordination mechanism is needed.  Thus authors of combinators
       * generally need not consider cancellation-specific semantics.
       * Such style is discouraged anyway.
       */
    
    (sapling split of b23361f054a9369a085bf3fb129253f223c38edc)

--- birdcage/util/util-core/src/main/scala/com/twitter/util/TimeoutException.scala
+++ birdcage/util/util-core/src/main/scala/com/twitter/util/TimeoutException.scala
@@ -1,7 +1,3 @@
 package com.twitter.util
 
-import java.util.concurrent.{TimeoutException => JUCTimeoutException}
-
-// Now that this is inherits from the usual TimeoutException, we can move to
-// j.u.c.TimeoutException during our next API break.
-class TimeoutException(message: String) extends JUCTimeoutException(message)
+class TimeoutException(message: String) extends Exception(message)
