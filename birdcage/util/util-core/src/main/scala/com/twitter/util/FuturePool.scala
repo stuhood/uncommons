2f0a248e544432a504ee2e0bc79b93d70b1b47c6
Robey Pointer <robey@twitter.com>
2011-09-12 12:17:53 -0700

    my laptop needs more time to finish this test
    (sapling split of 208746dbd7e8400c1d4a6aab93130ccb42613732)

--- birdcage/util/util-core/src/main/scala/com/twitter/util/FuturePool.scala
+++ birdcage/util/util-core/src/main/scala/com/twitter/util/FuturePool.scala
@@ -1,6 +1,6 @@
 package com.twitter.util
 
-import java.util.concurrent.{ExecutorService, Executors}
+import java.util.concurrent.ExecutorService
 
 /**
  * A FuturePool executes tasks asynchronously, typically using a pool
@@ -23,13 +23,6 @@ object FuturePool {
   val immediatePool = new FuturePool {
     def apply[T](f: => T) = Future(f)
   }
-
-  /**
-   * A default shared FuturePool, configured at 2 times the number of cores, similar to Netty. Convenient, but higher demand systems should configure their own.
-   */
-  lazy val defaultPool = new ExecutorServiceFuturePool(
-    Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2)
-  )
 }
 
 /**
@@ -39,14 +32,7 @@ class ExecutorServiceFuturePool(val executor: ExecutorService) extends FuturePoo
   def apply[T](f: => T): Future[T] = {
     val out = new Promise[T]
     executor.submit(new Runnable {
-      def run = {
-
-        // Make an effort to skip work in the case the promise
-        // has been cancelled or already defined.
-        if (!out.isDefined && !out.isCancelled) {
-          out.updateIfEmpty(Try(f))
-        }
-      }
+      def run = out.update(Try(f))
     })
     out
   }
