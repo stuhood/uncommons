6fd53f9f23efc9f5a5084cc84e3c3010da1eb605
marius a. eriksen <marius@twitter.com>
2012-02-24 10:34:05 -0800

    new failfast, option expFailFast()
    (sapling split of 9e4213d5c89c88da883acdc301d6ab6499b39e96)

--- birdcage/finagle/finagle-core/src/main/scala/com/twitter/finagle/service/FailFastFactory.scala
+++ birdcage/finagle/finagle-core/src/main/scala/com/twitter/finagle/service/FailFastFactory.scala
@@ -3,48 +3,76 @@ package com.twitter.finagle.service
 import java.net.ConnectException
 import java.util.concurrent.atomic.AtomicInteger
 import com.twitter.finagle.{FailFastException, WriteException, ServiceFactory, ServiceFactoryProxy, ClientConnection}
-import com.twitter.util.Future
+import com.twitter.finagle.stats.StatsReceiver
+import com.twitter.util.{Timer, Future, Duration, Time}
+import com.twitter.conversions.time._
 
+private[finagle] object FailFastFactory {
+  sealed trait State
+  case object Ok extends State
+  case class Retrying(since: Time, ntries: Int, backoffs: Stream[Duration]) extends State
+  
+  val defaultBackoffs = Backoff.exponential(1.second, 2).take(6)
+}
 
-/**
- * ServiceFactory Proxy responsible for handling fast-fail behavior when hosts are unreachable
- */
-class FailFastFactory[Req, Rep](
-  underlying: ServiceFactory[Req, Rep],
-  maxPending: Int = 1
-) extends ServiceFactoryProxy(underlying) {
+private[finagle] class FailFastFactory[Req, Rep](
+  self: ServiceFactory[Req, Rep],
+  statsReceiver: StatsReceiver,
+  timer: Timer,
+  backoffs: Stream[Duration] = FailFastFactory.defaultBackoffs
+    // Arbitrary but reasonable:
+    
+) extends ServiceFactoryProxy(self) {
+  import FailFastFactory._
 
-  // factory state
-  @volatile private[service] var isLimited = false
-  private[this] val pending = new AtomicInteger(0)
-
-  /**
-   * - If we are not limited: Just go through, every connection exception toggle the isLimited flag to true.
-   * - If we are limited: Acquire a "Token" (AtomicInteger) that allow you to establish a connection. If the connection
-   *   succeed the isLimited flag is set back to false.
-   */
-  override def apply(conn: ClientConnection) =
-    if (isLimited) {
-      val accept = pending.incrementAndGet() <= maxPending
-      if (accept) {
-        underlying(conn) onSuccess { _ =>
-          isLimited = false
-        } ensure {
-          pending.decrementAndGet()
-        }
-      } else {
-        pending.decrementAndGet()
-        Future.exception(new FailFastException)
-      }
-    } else {
-      underlying(conn) onFailure {
-        case WriteException(_) => isLimited = true
-        case _ => // Other exceptions don't change the factory state
+  @volatile private[this] var state: State = Ok
+  
+  private[this] val unhealthyForMsGauge =
+    statsReceiver.addGauge("unhealthy_for_ms") {
+      state match {
+        case Retrying(since, _, _) => since.untilNow.inMilliseconds
+        case _ => 0
       }
     }
 
-  override def isAvailable =
-    underlying.isAvailable && pending.get < maxPending
+  private[this] val unhealthyNumRetriesGauge = 
+    statsReceiver.addGauge("unhealthy_num_tries") {
+      state match {
+        case Retrying(_, ntries, _) => ntries
+        case _ => 0
+      }
+    }
 
-  override val toString = "fail_fast_%s".format(underlying.toString)
+  private[this] def reconnect(conn: ClientConnection) {
+    state match {
+      case Ok | Retrying(_, _, Stream.Empty) =>
+        state = Ok
+      case Retrying(since, ntries, wait #:: rest) =>
+        val nextState = Retrying(since, ntries+1, rest)
+        state = nextState
+        timer.schedule(wait) {
+          self(conn) onSuccess { _ =>
+            if (state == nextState) state = Ok
+          } onFailure { _ => 
+            if (state == nextState) reconnect(conn)
+          }
+        }
+      }
+  }
+
+  override def apply(conn: ClientConnection) =
+    self(conn) onFailure { _ =>
+      state match {
+        case Ok =>
+          state = Retrying(Time.now, 0, backoffs)
+          reconnect(conn)
+        case _ => // Already retrying
+      }
+    } onSuccess { _ =>
+      state = Ok
+    }
+
+  override def isAvailable = self.isAvailable && state == Ok
+
+  override val toString = "fail_fast_%s".format(self.toString)
 }
