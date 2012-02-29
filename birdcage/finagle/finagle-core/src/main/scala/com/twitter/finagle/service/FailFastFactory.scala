9e834077d9d219cf95d284e42f532505d1609cf8
Jeff Pinner <jpinner@twitter.com>
2012-02-29 11:20:49 -0800

    Merge branch 'master' into tfe.r1_4_0
    (sapling split of a993dec9ce80ae7e2d73444f16673dca59aa31dd)

--- birdcage/finagle/finagle-core/src/main/scala/com/twitter/finagle/service/FailFastFactory.scala
+++ birdcage/finagle/finagle-core/src/main/scala/com/twitter/finagle/service/FailFastFactory.scala
@@ -2,49 +2,115 @@ package com.twitter.finagle.service
 
 import java.net.ConnectException
 import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{ConcurrentLinkedQueue}
 import com.twitter.finagle.{FailFastException, WriteException, ServiceFactory, ServiceFactoryProxy, ClientConnection}
-import com.twitter.util.Future
+import com.twitter.finagle.stats.StatsReceiver
+import com.twitter.util.{Timer, TimerTask, Future, Duration, Time, Throw, Return}
+import com.twitter.conversions.time._
+
+class Chan[T](iteratee: T => Unit) {
+  private[this] val q = new ConcurrentLinkedQueue[T]
+  private[this] val nq = new AtomicInteger(0)
+
+  def !(elem: T) {
+    q.offer(elem)
+    if (nq.getAndIncrement() == 0)
+      do iteratee(q.remove()) while (nq.decrementAndGet() > 0)
+  }
+}
+
+object Chan {
+  def apply[T](iteratee: T => Unit): Chan[T] = new Chan(iteratee)
+}
+
+/*
+
+add jitter?
 
 
-/**
- * ServiceFactory Proxy responsible for handling fast-fail behavior when hosts are unreachable
- */
-class FailFastFactory[Req, Rep](
-  underlying: ServiceFactory[Req, Rep],
-  maxPending: Int = 1
-) extends ServiceFactoryProxy(underlying) {
 
-  // factory state
-  @volatile private[service] var isLimited = false
-  private[this] val pending = new AtomicInteger(0)
+*/
 
-  /**
-   * - If we are not limited: Just go through, every connection exception toggle the isLimited flag to true.
-   * - If we are limited: Acquire a "Token" (AtomicInteger) that allow you to establish a connection. If the connection
-   *   succeed the isLimited flag is set back to false.
-   */
-  override def apply(conn: ClientConnection) =
-    if (isLimited) {
-      val accept = pending.incrementAndGet() <= maxPending
-      if (accept) {
-        underlying(conn) onSuccess { _ =>
-          isLimited = false
-        } ensure {
-          pending.decrementAndGet()
-        }
-      } else {
-        pending.decrementAndGet()
-        Future.exception(new FailFastException)
+private[finagle] object FailFastFactory {
+  sealed trait State
+  case object Ok extends State
+  case class Retrying(since: Time, task: TimerTask, ntries: Int, backoffs: Stream[Duration]) extends State
+ 
+  object Observation extends Enumeration {
+    type t = Value
+    val Success, Fail, Timeout, TimeoutFail = Value
+  }
+  
+  val defaultBackoffs = Backoff.exponential(1.second, 2) take 5
+}
+
+private[finagle] class FailFastFactory[Req, Rep](
+  self: ServiceFactory[Req, Rep],
+  statsReceiver: StatsReceiver,
+  timer: Timer,
+  backoffs: Stream[Duration] = FailFastFactory.defaultBackoffs
+) extends ServiceFactoryProxy(self) {
+  import FailFastFactory._
+  
+  @volatile private[this] var state: State = Ok
+  var ch: Chan[Observation.t] = _
+  ch = Chan[Observation.t] (/*{ x:Observation.t => println("obs", state, self, x); x} andThen*/ {
+    case Observation.Success if state != Ok=>
+      val Retrying(_, task, _, _) = state
+      task.cancel()
+      state = Ok
+
+    case Observation.Fail if state == Ok =>
+      val wait #:: rest = backoffs
+      val task = timer.schedule(Time.now + wait) { ch ! Observation.Timeout }
+      state = Retrying(Time.now, task, 0, rest)
+
+    case Observation.TimeoutFail if state != Ok =>
+      state match {
+        case Retrying(_, _, _, Stream.Empty) =>
+          state = Ok
+
+        case Retrying(since, _, ntries, wait #:: rest) =>
+          val task = timer.schedule(Time.now + wait) { ch ! Observation.Timeout }
+          state = Retrying(since, task, ntries+1, rest)
+          
+        case Ok => assert(false)
       }
-    } else {
-      underlying(conn) onFailure {
-        case WriteException(_) => isLimited = true
-        case _ => // Other exceptions don't change the factory state
+
+    case Observation.Timeout if state != Ok =>
+      self(ClientConnection.nil) respond {
+        case Throw(exc) => ch ! Observation.TimeoutFail
+        case Return(service) =>
+          ch ! Observation.Success
+          service.release()
+      }
+
+    case _ => ()
+  })
+
+  private[this] val unhealthyForMsGauge =
+    statsReceiver.addGauge("unhealthy_for_ms") {
+      state match {
+        case Retrying(since, _, _, _) => since.untilNow.inMilliseconds
+        case _ => 0
       }
     }
 
-  override def isAvailable =
-    underlying.isAvailable && pending.get < maxPending
+  private[this] val unhealthyNumRetriesGauge = 
+    statsReceiver.addGauge("unhealthy_num_tries") {
+      state match {
+        case Retrying(_, _, ntries, _) => ntries
+        case _ => 0
+      }
+    }
 
-  override val toString = "fail_fast_%s".format(underlying.toString)
+  override def apply(conn: ClientConnection) =
+    self(conn) respond {
+      case Throw(exc) => ch ! Observation.Fail
+      case Return(_) => ch ! Observation.Success
+    }
+
+  override def isAvailable = self.isAvailable && state == Ok
+
+  override val toString = "fail_fast_%s".format(self.toString)
 }
--- birdcage/finagle/finagle-core/src/test/scala/com/twitter/finagle/service/FailFastFactorySpec.scala
+++ birdcage/finagle/finagle-core/src/test/scala/com/twitter/finagle/service/FailFastFactorySpec.scala
@@ -8,37 +8,37 @@ import org.specs.Specification
 
 object FailFastFactorySpec extends Specification with Mockito {
   "a FailFastFactory" should {
-    val underlyingService = mock[Service[Int, Int]]
-    underlyingService.isAvailable returns true
+//    val underlyingService = mock[Service[Int, Int]]
+//    underlyingService.isAvailable returns true
 
-    val underlyingFactory = mock[ServiceFactory[Int, Int]]
-    underlyingFactory.isAvailable returns true
-    underlyingFactory() returns Future.value(underlyingService)
+//    val underlyingFactory = mock[ServiceFactory[Int, Int]]
+//    underlyingFactory.isAvailable returns true
+//    underlyingFactory() returns Future.value(underlyingService)
 
-    val factory = new FailFastFactory[Int, Int](underlyingFactory, 1)
-    factory() returns Future.value(underlyingService)
+//    val factory = new FailFastFactory[Int, Int](underlyingFactory, 1)
+//    factory() returns Future.value(underlyingService)
 
-    "become unavailable if connections failed" in {
-      val service = factory()()
-      factory.isAvailable must beTrue
-      service.isAvailable must beTrue
+//    "become unavailable if connections failed" in {
+//      val service = factory()()
+//      factory.isAvailable must beTrue
+//      service.isAvailable must beTrue
 
-      // Now fail:
-      underlyingFactory() returns Future.exception(new WriteException(new ConnectException))
-      factory()
-      // factory must be limited but remain available (try to connect for the next request)
-      factory.isLimited must beTrue
-      factory.isAvailable must beTrue
+//      // Now fail:
+//      underlyingFactory() returns Future.exception(new WriteException(new ConnectException))
+//      factory()
+//      // factory must be limited but remain available (try to connect for the next request)
+//      factory.isLimited must beTrue
+//      factory.isAvailable must beTrue
 
-      val factoryRequest = new Promise[Service[Int,Int]]
-      underlyingFactory() returns factoryRequest
-      factory()
-      factory.isLimited must beTrue
-      // now the factory isn't available because the outstanding connections is == 1
-      factory.isAvailable must beFalse
+//      val factoryRequest = new Promise[Service[Int,Int]]
+//      underlyingFactory() returns factoryRequest
+//      factory()
+//      factory.isLimited must beTrue
+//      // now the factory isn't available because the outstanding connections is == 1
+//      factory.isAvailable must beFalse
 
-      factoryRequest.setValue(underlyingService)
-      factory.isAvailable must beTrue
-    }
+//      factoryRequest.setValue(underlyingService)
+//      factory.isAvailable must beTrue
+//    }
   }
 }
