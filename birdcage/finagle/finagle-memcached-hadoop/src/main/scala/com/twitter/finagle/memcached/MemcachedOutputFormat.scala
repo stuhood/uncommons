fcb07265d2de249bf49462181631e4a959c78faf
Kyle Maxwell <kyle@twitter.com>
2012-02-29 16:06:23 -0800

    merge hadoop
    (sapling split of 263fed0c677e47f886a35d0829e2e5e11448b51f)

--- birdcage/finagle/finagle-memcached-hadoop/src/main/scala/com/twitter/finagle/memcached/MemcachedOutputFormat.scala
+++ birdcage/finagle/finagle-memcached-hadoop/src/main/scala/com/twitter/finagle/memcached/MemcachedOutputFormat.scala
@@ -96,7 +96,7 @@ class MemcachedOutputFormat extends OutputFormat[Text, BytesWritable] {
       } onFailure { throwable =>
         retries.incrementAndGet()
         pending.decrementAndGet()
-        timer.doLater(MIN_SLEEP * (1 << tries))(write(key, value, tries + 1))
+        timer.doLater(MIN_SLEEP * (1 << tries))(write(key, value, tries + 1))        
       }
     }
 
