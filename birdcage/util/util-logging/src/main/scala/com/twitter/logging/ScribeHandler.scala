d5b3ef8bd5745877037b7ee4f36f2a5a1e8cebc3
devprod-git <devprod-git@twitter.com>
2014-11-07 20:55:43 +0000

    Merge branch 'birdcage/master'
    (sapling split of 71b7b2905c91b90954fc5b7a0bcd25a7763f2f27)

--- birdcage/util/util-logging/src/main/scala/com/twitter/logging/ScribeHandler.scala
+++ birdcage/util/util-logging/src/main/scala/com/twitter/logging/ScribeHandler.scala
@@ -99,9 +99,11 @@ class ScribeHandler(
     _lastLogStats = Time.now
   }
 
-  @volatile private var _lastTransmission = Time.epoch
+  private var _lastTransmission = Time.epoch
   // visible for testing
-  private[logging] def updateLastTransmission(): Unit = _lastTransmission = Time.now
+  private[logging] def updateLastTransmission(): Unit = synchronized {
+    _lastTransmission = Time.now
+  }
 
   private var socket: Option[Socket] = None
   private var archaicServer = false
@@ -145,7 +147,7 @@ class ScribeHandler(
         val skipped = reconnectionSkipped.getAndSet(0)
         log.info("sent records: %d, per second: %d, dropped records: %d, reconnection failures: %d, reconnection skipped: %d",
                  sent, sent/period.inSeconds, dropped, failed, skipped)
-        _lastLogStats = Time.now
+        updateLastLogStats()
       }
     }
 
